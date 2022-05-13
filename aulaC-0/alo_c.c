86edd2961a50da0ad78e49e86ace9bbf52de3b70
jobensino <joinusmaker@gmail.com>
2022-05-13 15:51:54 -0300

    Create alo_c.c

---
+++ aulaC-0/alo_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+   Autor
+*/
+
+// função principal
+int main()
+{
+	printf("Alo programador C"); // escreva
+	
+	printf("\nPressione uma tecla para continuar... \n");
+	getchar();
+}
+
+
