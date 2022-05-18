f61fd62e520f4ac35beaa60e44e93eba3677b59a
Josildo Silva <74023029+jobensino@users.noreply.github.com>
2022-05-18 17:59:20 -0300

    Add files via upload

---
+++ aulaC-0/alo_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+   Autor
+*/
+
+// funçao criada pelo programador
+void meu_escreva(){
+	printf("\n\n....Usando MEU_ESCREVA...\n\n");
+}
+
+// função principal
+int main()
+{
+	printf("Alo programador C"); // escreva
+	
+	meu_escreva();
+	
+	printf("\nPressione uma tecla para continuar... \n");
+	getchar();
+	
+	return 0;
+}
+
+
